b6b7e23d8da1d4d06798d29f64ac6fc2094a3bd6
nsararaa <149585185+nsararaa@users.noreply.github.com>
2024-05-29 10:02:15 +0500

    Initial Commit

---
+++ LMS/LMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMSApp.swift
+//  LMS
+//
+//  Created by Sara Noor on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
